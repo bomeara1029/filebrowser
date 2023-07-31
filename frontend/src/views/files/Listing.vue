f3da68e767a99b025dbf1d7cd8475936e065e6a7
Ben O'Meara <bomeara@cjmoyna.com>
2023-07-31 12:57:49 -0500

    Updated edit scene icon and added dynamic text based on whether there is an existing scene in the current folder

--- frontend/src/views/files/Listing.vue
+++ frontend/src/views/files/Listing.vue
@@ -51,7 +51,7 @@
           :label="$t('buttons.explore')"
           @action="explore"
         />
-        <action :icon="edit" :label="$t('buttons.story')" @action="editStory" />
+        <action icon="stars" :label="hasScene?$t('buttons.editScene'):$t('buttons.createScene')" @action="editStory" />
         <action
           v-if="headerButtons.shell"
           icon="code"
